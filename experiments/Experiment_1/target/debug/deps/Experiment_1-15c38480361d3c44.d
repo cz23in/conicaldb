e32076d0ed6f5210789a0d0ebaa469cea8145849
cz23in <cz23in@aol.com>
2023-10-14 15:05:36 +0200

    Added expierment descriptions

---
+++ experiments/Experiment_1/target/debug/deps/Experiment_1-15c38480361d3c44.d
@@ -0,0 +1,5 @@
+/home/desktop/Files/conicaldb/experiments/Experiment_1/target/debug/deps/Experiment_1-15c38480361d3c44.rmeta: src/main.rs
+
+/home/desktop/Files/conicaldb/experiments/Experiment_1/target/debug/deps/Experiment_1-15c38480361d3c44.d: src/main.rs
+
+src/main.rs:
---
+++ experiments/Experiment_1/target/debug/deps/Experiment_1-217f1b3fe99f8a27.d
@@ -0,0 +1,5 @@
+/home/desktop/Files/conicaldb/experiments/Experiment_1/target/debug/deps/Experiment_1-217f1b3fe99f8a27.rmeta: src/main.rs
+
+/home/desktop/Files/conicaldb/experiments/Experiment_1/target/debug/deps/Experiment_1-217f1b3fe99f8a27.d: src/main.rs
+
+src/main.rs:
